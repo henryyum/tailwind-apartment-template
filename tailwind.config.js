9caf8e64bf8842d337b520046d147ffb6d0f12bd
Henry Yum <henry.yum1993@hotmail.com>
2024-08-02 15:14:31 +1000

    added fslightbox and smooth scroll

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,41 @@ module.exports = {
   content: ['./dist/*.{html,js}'],
   theme: {
     extend: {
-    
+      spacing: {
+        'custom-container': '1980px',
+      }
     },
   },
-  plugins: [],
-}
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        '.custom-container': {
+          maxWidth: '100%',
+          '@screen sm': {
+            maxWidth: '640px',
+          },
+          '@screen md': {
+            maxWidth: '768px',
+          },
+          '@screen lg': {
+            maxWidth: '1024px',
+          },
+          '@screen xl': {
+            maxWidth: '1280px',
+          },
+          '@screen 2xl': {
+            maxWidth: '1980px',
+          },
+        }
+      })
+    },
+    function ({ addUtilities}) {
+      addUtilities({
+        '.scroll-smooth': {
+          'scroll-behavior': "smooth",
+        },
+      });
+    },
+  ],
+};
 
